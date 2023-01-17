67be78278303c5b9c3181abda7ede77a8c7cf6b0
Yorch Moreno <maicolmoreno_18@outlook.com>
2023-01-16 19:17:49 -0500

    Dandole nombre a una variable

---
+++ src/PrimerEjercicio.java
@@ -0,0 +1,6 @@
+public class PrimerEjercicio {
+    public static void main (String[] args){
+        String nombre = "George Michael Moreno Beltran";
+        System.out.println("Mi nombre es "+nombre);
+    }
+}
